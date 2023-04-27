b63543fd439d079155bf50461671cb002e5d6ca2
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-27 03:20:39 -0700

    Create gridSquares container inside sketchContainer.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
   const sketchContainer = document.createElement('div');
   sketchContainer.classList.add('sketchContainer');
-  
   document.body.appendChild(sketchContainer);
 
+  const gridSquares = document.createElement('div');
+  gridSquares.classList.add('gridSquares');
+  sketchContainer.appendChild(gridSquares);
