8de32754745a748fb431b5effe2954d25c79ebae
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-27 18:22:12 -0700

    Redo div container. CreateGrid() to include looping squareBox into row. gridSize parameter determines gridSize. Style squareBox. Add display: flex to row for correct layout.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-  const sketchContainer = document.createElement('div');
-  sketchContainer.classList.add('sketchContainer');
-  document.body.appendChild(sketchContainer);
+const container = document.querySelector('.container');
+const gridSize = 4;
+//const sketchContainer = document.createElement('div');
+//sketchContainer.classList.add('sketchContainer');
+//ontainer.appendChild(sketchContainer);
 
-  const gridSquares = document.createElement('div');
-  gridSquares.classList.add('gridSquares');
-  sketchContainer.appendChild(gridSquares);
+//const gridSquares = document.createElement('div');
+//gridSquares.classList.add('gridSquares');
+//sketchContainer.appendChild(gridSquares);
+
+const createGrid = (grid) => {
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row')
+
+    for (let j = 0; j < grid; j++) {
+      const squareBox = document.createElement('div');
+      squareBox.classList.add('squareBox');
+      row.appendChild(squareBox);
+    }
+
+    container.appendChild(row);
+  }
+}
+
+createGrid(gridSize);
